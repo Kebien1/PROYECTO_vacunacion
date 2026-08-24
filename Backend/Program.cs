2b6995260faa581e9450281fe830379410cef92a
Kevin <nunezortizkevin@gmail.com>
2026-08-24 13:34:01 -0400

    instalacion de recursos

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("Frontend", policy => policy
-        .WithOrigins("http://localhost:5174", "http://localhost:4173")
+        .WithOrigins("http://localhost:5173", "http://localhost:5174")
         .AllowAnyHeader()
         .AllowAnyMethod());
 });
